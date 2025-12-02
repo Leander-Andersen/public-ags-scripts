a241b441cb585470302813ab168d40e97e1d0bdf
Leander-Andersen <116058453+Leander-Andersen@users.noreply.github.com>
2025-12-02 10:27:14 +0100

    Add .vscode and .github to ignored files list
    Updated the $ignore array in globalVariables.php to exclude .vscode and .github directories from file listings.

--- ##Extras/globalVariables.php
+++ ##Extras/globalVariables.php
@@ -1,6 +1,6 @@
 <?php
 
 // Files to ignore in the listing
-$ignore = array('..', '.', 'index.php', 'indexu.php','indexu.css','.gitattributes', '.git','.htaccess', 'globalVariables.php', '##Extras'  );
+$ignore = array('..', '.', 'index.php', 'indexu.php','indexu.css','.gitattributes', '.git','.htaccess', 'globalVariables.php','##Extras','.vscode','.github');
 
 ?>
